237dca5e2716649d838be765e95a0c17337ce2ed
pushpam patel <30745430+pushpam-patel@users.noreply.github.com>
2019-11-02 18:25:23 +0530

    register

---
+++ manish_db-master/js/register.js
@@ -0,0 +1,23 @@
+document.querySelector('#reg_b').addEventListener('click',(e)=>{
+    let name = document.querySelector('#username').value
+    let pass = document.querySelector('#upass').value
+    let email = document.querySelector('#email').value
+    let edu = document.querySelector('#edu').value
+    let age = document.querySelector('#age').value
+    let uni = document.querySelector('#uni').value
+    
+    let new_user = {username: name, password:pass,age:Number(age),email:email,edu:edu,uni:uni}
+
+    axios.post('https://www.localhost/users',new_user,{
+        headers:{
+            'Content-type':'application/json'
+        }
+    }).then((res)=>{
+        console.log(res.data)
+        console.log(emaila)
+        window.location.href = "login.html"
+    })
+
+
+    }
+)
