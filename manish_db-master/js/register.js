43580f70114a0b79ca37d1bc91ff3ac0cb2e119e
pushpam patel <30745430+pushpam-patel@users.noreply.github.com>
2019-11-02 18:49:30 +0530

    sec

--- manish_db-master/js/register.js
+++ manish_db-master/js/register.js
@@ -5,10 +5,10 @@ document.querySelector('#reg_b').addEventListener('click',(e)=>{
     let edu = document.querySelector('#edu').value
     let age = document.querySelector('#age').value
     let uni = document.querySelector('#uni').value
-    
+
     let new_user = {username: name, password:pass,age:Number(age),email:email,edu:edu,uni:uni}
 
-    axios.post('https://www.localhost/users',new_user,{
+    axios.post('https://localhost:3000/users',new_user,{
         headers:{
             'Content-type':'application/json'
         }
